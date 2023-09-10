2cf54393268e1a4699aaf3e63bfc44a8c3908b81
Lali <lalithyanawanjana@gmail.com>
2023-09-10 17:02:53 +0530

    Preview Page
    Create preview page

---
+++ WealthWave/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  WealthWave
+//
+//  Created by Lali.. on 10/09/2023.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Hello, Lali!")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ WealthWave/Views/Preview.swift
@@ -0,0 +1,45 @@
+//
+//  Preview.swift
+//  WealthWave
+//
+//  Created by Lali.. on 10/09/2023.
+//
+
+import SwiftUI
+
+struct Preview: View {
+    @State private var isLoginViewActive = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                NavigationLink("", destination: LoginView(), isActive: $isLoginViewActive)
+                
+                Image("LOGO")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                
+                HStack {
+                    Text("WealthWave")
+                        .font(.system(size: 24))
+                        .foregroundColor(Color(hue: 0.666, saturation: 1.0, brightness: 0.57))
+                }
+                HStack{
+                    Text("Ride the Wave to Financial Prosperity!....")
+                        .foregroundColor(Color(hue: 0.666, saturation: 1.0, brightness: 0.57))
+                }
+            }
+            .onTapGesture {
+                isLoginViewActive = true
+            }
+        }
+    }
+}
+
+struct Preview_Previews: PreviewProvider {
+    static var previews: some View {
+        Preview()
+    }
+}
+
