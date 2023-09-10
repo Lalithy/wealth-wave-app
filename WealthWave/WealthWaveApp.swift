d85ef9c15feb556b2f00f09600209f10ac38a525
Lali <lalithyanawanjana@gmail.com>
2023-09-10 14:52:17 +0530

    Initial Commit

---
+++ WealthWave/WealthWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  WealthWaveApp.swift
+//  WealthWave
+//
+//  Created by Lali.. on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct WealthWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
