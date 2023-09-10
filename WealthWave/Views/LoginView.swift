c649946db0494e151aaa3d9427fcaf0165206f9d
Lali <lalithyanawanjana@gmail.com>
2023-09-10 19:12:23 +0530

    Login Page
    Creating a Login Page

--- WealthWave/Views/LoginView.swift
+++ WealthWave/Views/LoginView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var username = ""
+    @State private var password = ""
+    
+    let gradient = Gradient(colors: [Color("ButtonColourTop"), Color("ButtonColourMiddle"), Color("ButtonColourEnd")])
+    
     var body: some View {
-        Text("Hello, Lali!")
+  
+            VStack{
+                            HStack {
+                                Spacer()
+                                Image("LOGO")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 70, height: 70)
+                                    .padding(.top, 20)
+                                Spacer()
+                            }
+                Spacer().frame(height:40)
+                HStack{
+                    Text("Sign up with your email address")
+                        .bold()
+                }
+                
+               Spacer().frame(height:40)
+               TextField("Username", text: $username)
+                    .padding()
+                    .frame(width: 300)
+                    .background(Color.black.opacity(0.05))
+                    .cornerRadius(15)
+                
+                Spacer().frame(height:20)
+                TextField("Password", text: $password)
+                     .padding()
+                     .frame(width: 300)
+                     .background(Color.black.opacity(0.05))
+                     .cornerRadius(15)
+                
+                Spacer().frame(height:50)
+                Button("Login"){
+                    
+                }
+                .foregroundColor(.white)
+                .frame(width: 300, height: 50)
+                .bold()
+                .background(LinearGradient(gradient: gradient, startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(10)
+                
+                Spacer()
+                
+            }
+        
+        
     }
 }
 
