94c5294891540fa88767f70d99c9aaeefd3c28e3
Lali <lalithyanawanjana@gmail.com>
2023-09-24 22:27:39 +0530

    Fixed
    Get income

--- WealthWave/Views/AddItemsView.swift
+++ WealthWave/Views/AddItemsView.swift
@@ -18,11 +18,14 @@ struct AddItemsView: View {
                 TabBarButton(title: "EXPENSES", isSelected: selectedTab == 0) {
                     selectedTab = 0
                 }
-                TabBarButton(title: "INCOME", isSelected: selectedTab == 1) {
-                    selectedTab = 1
-                }
+//                TabBarButton(title: "INCOME", isSelected: selectedTab == 1) {
+//                    selectedTab = 1
+//                }
                 TabBarButton(title: "SAVINGS", isSelected: selectedTab == 2) {
                     selectedTab = 2
+                }
+                TabBarButton(title: "INCOME", isSelected: selectedTab == 3) {
+                    selectedTab = 3
                 }
                 
             }
@@ -33,9 +36,11 @@ struct AddItemsView: View {
                 
                 ExpensesView()
                 
-                IncomeView()
+                //IncomeView()
                 
                 SavingView()
+                
+                ExpensesView1()
                 
                 
             }
@@ -87,7 +92,7 @@ struct ExpensesView: View {
                         UserExpensesListView(iconName: "minus.circle.fill",image: item.expenseCategory, expenseCategory: item.expenseCategory, expenseAmount: item.expenseAmount, expenseDetails: item.expenseDetails, expenseDate: item.expenseDate)
                         
                     }
-                    Spacer()
+                    //Spacer()
                 }
 //            }
             
@@ -144,7 +149,7 @@ struct UserExpensesListView: View {
                 Image(image)
                     .resizable()
                     .frame(width: 50, height: 50)
-                    .padding(.leading, 10)
+                    //.padding(.leading, 10)
                     .scaledToFit()
                 
                 Text(expenseCategory)
@@ -153,14 +158,12 @@ struct UserExpensesListView: View {
                 
                 Text(String(format: "%.2f", expenseAmount))
                     .font(.system(size: 20))
-                    .padding(.leading, 10)
+                    //.padding(.leading, 10)
                 //Spacer()
                 
             }
-            Spacer()
+            //Spacer()
             HStack {
-               
-                
                 Text(expenseDetails)
                     .font(.system(size: 20))
                     //.padding(.leading, 10)
@@ -169,9 +172,7 @@ struct UserExpensesListView: View {
                     .font(.system(size: 20))
                     //.padding(.leading, 10)
                 //Spacer()
-                
             }
-           
             
         }
         .padding(.top, 10)
@@ -192,7 +193,7 @@ struct IncomeView: View {
                     .font(.system(size: 25))
                     .bold()
                     .padding(.top, 10)
-                
+
                 HStack {
                     Text("Date")
                         .font(.headline)
@@ -205,15 +206,15 @@ struct IncomeView: View {
                         .padding(.leading, 30)
                 }
                 .padding(.top, 10)
-                
+
                 ScrollView {
                     LazyVStack {
                         ForEach(incomeViewModel.incomeData, id: \.incomeId) { item in
-                            
+
                             VStack {
-                                
+
                                 HStack {
-                                    
+
                                     Text(item.incomeDate)
                                         .padding(.trailing, 30)
                                     Text(item.incomeDetails)
@@ -223,18 +224,18 @@ struct IncomeView: View {
                                 }
                             }
                         }
-                        
+
                     }
                 }
-                
-                
+
+
                 Spacer()
-                
+
                 NavigationLink(
                     destination: AddIncomeView(),
                     isActive: $isIncomeVisible
                 ) {}
-                
+
                 Button(action: {
                     isIncomeVisible = true
                 }) {
@@ -312,3 +313,104 @@ struct SavingView: View {
 }
 
 
+struct ExpensesView1: View {
+    
+    @StateObject var incomeViewModel: IncomeViewModel = IncomeViewModel()
+    @State private var isListVisible = false
+    
+    var body: some View {
+        VStack {
+            
+            Text("Total: 1000")
+                .font(.system(size: 25))
+                .bold()
+                .padding(.top, 10)
+
+            HStack {
+                Text("Date")
+                    .font(.headline)
+                    .padding(.trailing, 30)
+                Text("Description")
+                    .font(.headline)
+                    .padding(.trailing, 30)
+                Text("Amount")
+                    .font(.headline)
+                    .padding(.leading, 30)
+            }
+            .padding(.top, 10)
+            
+//            if userExpensesList.isLoading {
+//                ProgressView()
+//            } else {
+                ScrollView {
+                    ForEach(incomeViewModel.incomeData, id: \.incomeId) { item in
+                        UserExpensesListView1(incomeDate: item.incomeDate, incomeDetils: item.incomeDetails ,incomeAmount: item.incomeAmount)
+                        
+                    }
+                    //Spacer()
+                }
+//            }
+            
+            Spacer()
+            
+            NavigationLink(
+                destination: ListOfExpensesView(),
+                isActive: $isListVisible
+            ) {
+                
+            }
+            .hidden()
+            
+            Button(action: {
+                
+                isListVisible = true
+            }) {
+                Image(systemName: "plus.circle")
+                    .font(.system(size: 40))
+                    .foregroundColor(.blue)
+            }
+            .padding(.bottom, 20)
+            .padding(.trailing, 20)
+            
+            
+        }.tag(3)
+            .onAppear {
+                incomeViewModel.fetchIncomeData()
+            }
+    }
+}
+
+
+struct UserExpensesListView1: View {
+    
+    var incomeDate: String
+    var incomeDetils: String
+    var incomeAmount: Double
+    
+    var body: some View {
+        
+        VStack {
+            HStack {
+               
+                //.padding(.leading, 10)
+                
+                Text(incomeDate)
+                    .font(.system(size: 10))
+
+                Text(incomeDetils)
+                    .font(.system(size: 20))
+                
+                Text(String(format: "%.2f", incomeAmount))
+                    .font(.system(size: 20))
+                    //.padding(.leading, 10)
+                //Spacer()
+                
+            }
+            //Spacer()
+           
+            
+        }
+        .padding(.top, 10)
+        
+    }
+}
